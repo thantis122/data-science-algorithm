98149693370e3b393b8e394b61567f9bd24ba238
LIU Bin <liubin122@hotmail.com>
2018-03-20 20:32:06 +0800

    update

--- classification/decision_tree_app.py
+++ classification/decision_tree_app.py
@@ -5,6 +5,21 @@
 # DATE: 2018/3/20
 # --------------------------------------
 
+import numpy as np
+import pandas as pd
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+
+
+
 
 
 
